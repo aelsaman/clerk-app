0d6098527d8d74995aacfe6848e6699d59fec194
Alaa ElSaman <aelsaman0x@gmail.com>
2024-04-25 19:17:09 +0200

    change Header comp to use v5 syntax

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
-import { UserButton, auth } from '@clerk/nextjs';
+import { UserButton, SignedIn, SignedOut } from '@clerk/nextjs';
 
-const Header = async ({ username }) => {
-  const { userId } = auth();
+const Header = ({ username }) => {
+  // this hook now only works client side 
+  // const { userId } = useAuth();
 
   return (
     <nav className='flex items-center justify-between px-6 py-4 mb-5 bg-blue-700'>
@@ -14,27 +15,31 @@ const Header = async ({ username }) => {
         </Link>
       </div>
       <div className='flex items-center text-white'>
-        {!userId && (
+        {/* {!userId && ( */}
+        <SignedOut>
           <>
             <Link
               href='sign-in'
               className='text-gray-300 hover:text-white mr-4'
-            >
+              >
               Sign In
             </Link>
             <Link
               href='sign-up'
               className='text-gray-300 hover:text-white mr-4'
-            >
+              >
               Sign Up
             </Link>
           </>
-        )}
-        {userId && (
+        </SignedOut>
+        {/* )} */}
+        {/* {userId && ( */}
+        <SignedIn>
           <Link href='profile' className='text-gray-300 hover:text-white mr-4'>
             Profile
           </Link>
-        )}
+        </SignedIn>
+        {/* )} */}
         <div className='ml-auto'>
           <UserButton afterSignOutUrl='/' />
         </div>
