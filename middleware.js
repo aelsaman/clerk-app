4d95658d9def484b4ed77ce49a77955a92ca96e6
Alaa ElSaman <aelsaman0x@gmail.com>
2024-04-25 19:16:43 +0200

    use clerk middleware v5 syntax

--- middleware.js
+++ middleware.js
@@ -1,9 +1,11 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  publicRoutes: ['/', '/profile', '/register'],
+const isProtectedRoute = createRouteMatcher(['/dashboard', '/profile']);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 };
